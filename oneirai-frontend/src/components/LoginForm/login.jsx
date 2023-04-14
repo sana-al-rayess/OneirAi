9e4dac677368d2675d03d99331b20d6b95821200
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 01:32:00 +0300

    added flex container

--- oneirai-frontend/src/components/LoginForm/login.jsx
+++ oneirai-frontend/src/components/LoginForm/login.jsx
@@ -1,3 +1,10 @@
 import React, { useState } from "react";
 import Logo from "../../images/logo_oneira.png";
 import("./login.css");
+
+
+
+return (
+    <div className="flex">
+        <div className="flex">
+            
