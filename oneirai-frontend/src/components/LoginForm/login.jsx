23f86d581c65daf6ade0769ee62b6db656ac05be
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 02:02:22 +0300

    added validations to forms

--- oneirai-frontend/src/components/LoginForm/login.jsx
+++ oneirai-frontend/src/components/LoginForm/login.jsx
@@ -1,6 +1,3 @@
-import React, { useState } from "react";
-import Logo from "../../images/logo_oneira.png";
-import("./login.css");
 
 function LoginForm() {
 	const [email, setEmail] = useState("");
@@ -10,7 +7,7 @@ function LoginForm() {
 	const [passwordError, setPasswordError] = useState("");
 	const [nameError, setNameError] = useState("");
 
-    const handleSubmit = (e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 		let valid = true;
 		if (!name.trim()) {
@@ -32,75 +29,106 @@ function LoginForm() {
 			valid = false;
 		}
 		if (valid) {
-		
+			// submit the form or perform other actions
 			console.log("Form submitted");
 		}
 	};
 
+	return (
+		<div className="flex">
+			<div className="flex">
+				<a href="/" className="login-title-main white flex text-decoration-none">
+					<img className="logo" src={Logo} alt="logo" />
+				</a>
+			</div>
+			<div className="container">
+				<input type="checkbox" id="chk" aria-hidden="true" />
 
-return (
-    <div className="flex">
-        <div className="flex">
-            <a href="/" className=" flex">
-                <img className="logo" src={Logo} alt="logo" />
-            </a>
-        </div>
-        <div className="container">
-            <input type="checkbox" id="chk" aria-hidden="true" />
+				<div className="signup">
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="chk" aria-hidden="true">
+							Sign up
+						</label>
+						<input
+							type="text"
+							name="name"
+							placeholder="Name"
+							required
+							className={`input-with-user-icon ${nameError ? "error" : ""}`}
+							value={name}
+							onChange={(e) => {
+								setName(e.target.value);
+								setNameError("");
+							}}
+						/>
+						{nameError && <p className="error-message">{nameError}</p>}
+						<input
+							type="email"
+							name="email"
+							placeholder="Email"
+							required
+							className={`input-with-mail-icon ${emailError ? "error" : ""}`}
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+								setEmailError("");
+							}}
+						/>
+						{emailError && <p className="error-message">{emailError}</p>}
+						<input
+							type="password"
+							name="password"
+							placeholder="Password"
+							required
+							className={`input-with-lock-icon ${passwordError ? "error" : ""}`}
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+								setPasswordError("");
+							}}
+						/>
+						{passwordError && <p className="error-message">{passwordError}</p>}
+						<button>Sign up</button>
+					</form>
+				</div>
 
-            <div className="signup">
-                <form>
-                    <label htmlFor="chk" aria-hidden="true">
-                        Sign up
-                    </label>
-                    <input
-                        type="text"
-                        name="name"
-                        placeholder="Name"
-                        required
-                    />
-
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        required
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-                    />
-                    <button>Sign up</button>
-                </form>
-            </div>
-
-            <div className="login">
-                <form>
-                    <label htmlFor="chk" aria-hidden="true">
-                        Login
-                    </label>
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        required
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        required
-
-                    />
-                   
-                    <button>Login</button>
-                </form>
-            </div>
-        </div>
-    </div>
-);
-
+				<div className="login">
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="chk" aria-hidden="true">
+							Login
+						</label>
+						<input
+							type="email"
+							name="email"
+							placeholder="Email"
+							required
+							className={`input-with-mail-icon ${emailError ? "error" : ""}`}
+							value={email}
+							onChange={(e) => {
+								setEmail(e.target.value);
+								setEmailError("");
+							}}
+						/>
+						{/* <input type="password" name="pswd" placeholder="Password" required className="input-with-lock-icon"/> */}
+						<input
+							type="password"
+							name="password"
+							placeholder="Password"
+							required
+							className={`input-with-lock-icon ${passwordError ? "error" : ""}`}
+							value={password}
+							onChange={(e) => {
+								setPassword(e.target.value);
+								setPasswordError("");
+							}}
+						/>
+						{passwordError && <p className="error-message">{passwordError}</p>}
+						<button>Login</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+}
 
 export default LoginForm;
