dd09b109e901954964eb0d506040d5b5300611b5
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 01:40:28 +0300

    created signup form

--- oneirai-frontend/src/components/LoginForm/login.jsx
+++ oneirai-frontend/src/components/LoginForm/login.jsx
@@ -14,4 +14,30 @@ return (
         <div className="container">
 				<input type="checkbox" id="chk" aria-hidden="true" />
 
-				
+				<div className="signup">
+					<form>
+						<label htmlFor="chk" aria-hidden="true">
+							Sign up
+						</label>
+						<input
+							type="text"
+							name="name"
+							placeholder="Name"
+							required
+						/>
+						
+						<input
+							type="email"
+							name="email"
+							placeholder="Email"
+							required
+						/>
+						<input
+							type="password"
+							name="password"
+							placeholder="Password"
+							required
+						/>
+						<button>Sign up</button>
+					</form>
+				</div>
