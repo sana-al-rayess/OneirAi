3733556610ac086bee83f58bcdc963512e61cf0c
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 01:37:29 +0300

    added container and checkbox

--- oneirai-frontend/src/components/LoginForm/login.jsx
+++ oneirai-frontend/src/components/LoginForm/login.jsx
@@ -7,8 +7,11 @@ import("./login.css");
 return (
     <div className="flex">
         <div className="flex">
-            <a href="/" className="flex">
+            <a href="/" className=" flex">
                 <img className="logo" src={Logo} alt="logo" />
             </a>
         </div>
-        
+        <div className="container">
+				<input type="checkbox" id="chk" aria-hidden="true" />
+
+				
