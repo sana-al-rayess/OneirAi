1b7fd404ce5f0c4d9a7065018ef10a925e8b5cd8
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 01:41:48 +0300

    created login form

--- oneirai-frontend/src/components/LoginForm/login.jsx
+++ oneirai-frontend/src/components/LoginForm/login.jsx
@@ -12,32 +12,61 @@ return (
             </a>
         </div>
         <div className="container">
-				<input type="checkbox" id="chk" aria-hidden="true" />
+            <input type="checkbox" id="chk" aria-hidden="true" />
 
-				<div className="signup">
-					<form>
-						<label htmlFor="chk" aria-hidden="true">
-							Sign up
-						</label>
-						<input
-							type="text"
-							name="name"
-							placeholder="Name"
-							required
-						/>
-						
-						<input
-							type="email"
-							name="email"
-							placeholder="Email"
-							required
-						/>
-						<input
-							type="password"
-							name="password"
-							placeholder="Password"
-							required
-						/>
-						<button>Sign up</button>
-					</form>
-				</div>
+            <div className="signup">
+                <form>
+                    <label htmlFor="chk" aria-hidden="true">
+                        Sign up
+                    </label>
+                    <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        required
+                    />
+
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        required
+                    />
+                    <button>Sign up</button>
+                </form>
+            </div>
+
+            <div className="login">
+                <form>
+                    <label htmlFor="chk" aria-hidden="true">
+                        Login
+                    </label>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        required
+
+                    />
+                   
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+);
+
+
+export default LoginForm;
