b1596db35e2c2f71563dfd0a55f064121488c30a
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 23:48:01 +0300

    added rrest of cards to facts component

--- oneirai-frontend/src/components/Facts/index.jsx
+++ oneirai-frontend/src/components/Facts/index.jsx
@@ -14,7 +14,32 @@ const Facts = () => {
                     </div>
 
                 </div>
+                <div class="card">
+                    <h3 class="title">Fact 2</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+
                 </div>
+                <div class="card">
+                    <h3 class="title">Fact 3</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <h3 class="title">Fact 4</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
 
     );
