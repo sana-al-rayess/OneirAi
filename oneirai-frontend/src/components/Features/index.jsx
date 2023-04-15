26f7514ad16e5bbdc85fb557b2dbd734ac24f767
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 19:29:41 +0300

    created a features component

---
+++ oneirai-frontend/src/components/Features/index.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Button from "../Button";
+import Feature1 from "../../images/Mask.png";
+import Feature2 from "../../images/transai.png";
+import Feature3 from "../../images/7248902.png"
+import './features.css';
+
+const Features = () => {
+
+    return (
+        <div>
+            <p className="feature-title">FEATURES</p>
+            <div className="features-container">
+
+                <img className="photo" src={Feature1} alt="photo" />
+
+                <div className="feature-txt-button">
+                    <div><p className="feature1">Lorem Epsum Lorem Epsum <br />Lorem Epsum Lorem Epsum  </p></div>
+
+                    <div><Button>Try Now</Button></div>
+                </div>
+            </div>
+
+           
+        </div>
+    );
+
+};
+
+export default Features;
