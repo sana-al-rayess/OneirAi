61984256cc146fc2125b0e6543dae1fdead65f14
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 23:48:59 +0300

    added facts component to landing page

--- oneirai-frontend/src/Pages/Landing/index.jsx
+++ oneirai-frontend/src/Pages/Landing/index.jsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import Intro from "../../components/Intro";
 import Parallax from "../../components/Parallax";
 import Features from "../../components/Features";
+import Facts from "../../components/Facts";
 import Image1 from "../../images/starry.jpg";
 import Image2 from "../../images/starry2.jpg";
 import Image3 from "../../images/starry3.jpg";
@@ -24,6 +25,7 @@ const Landing = () => {
             <div> <Parallax backgroundImage={Image2}>
             </Parallax></div>
             <br/><br/>
+            <div> <Facts /> </div>
 
 
 
