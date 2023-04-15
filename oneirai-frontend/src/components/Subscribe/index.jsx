132225911808c3f27f22c1a9204f8eea6c6b3373
Sana Rayess <rayess.sana@gmail.com>
2023-04-16 00:44:40 +0300

    edited subscribe component

--- oneirai-frontend/src/components/Subscribe/index.jsx
+++ oneirai-frontend/src/components/Subscribe/index.jsx
@@ -3,11 +3,17 @@ import './subscribe.css';
 const Subscribe = () => {
 
     return (
-        <div>
-           
+        <div className="subscribe-container">
+          <p className="subscribe-title">Get In Touch</p>
+          <p className="subscribe-slogan">
+            Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero.
+          </p>
+          <form>
+            <input type="email" id="email" placeholder="Your email" />
+            <button className='subscribe-btn'>Subscribe</button>
+          </form>
         </div>
-
-    );
+      );
 
 };
 
