39609058f9920ea27e83361a55a71d3bfeef7171
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 13:10:18 +0300

    created landing page

---
+++ oneirai-frontend/src/Pages/Landing/index.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import './landing.css';
+
+
+
+
+
+
+const Landing = () => {
+    return (
+        <div className="landing">
+
+        </div>
+    );
+};
+
+export default Landing;
