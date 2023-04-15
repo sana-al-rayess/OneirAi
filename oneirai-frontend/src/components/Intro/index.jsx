d92bee2596d0c63e578da134373b2b552e93972d
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 15:47:05 +0300

    created an intro component

---
+++ oneirai-frontend/src/components/Intro/index.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Button from "../Button";
+
+import './intro.css';
+
+const Intro = () => {
+ 
+  };
+
+  return (
+
+    <div className="intro-container">
+      <div>
+        <p className="slogan-txt">Oneir<span>Ai</span>: where imagination<br /> meets AI interpretation.</p>
+      </div>
+      <div>
+        <p className="slogan-txt2">Unleash the power of AI to visualize and interpret<br /> your subconscious mind!</p></div>
+      <div>
+        <div>
+          <Button>Get Started</Button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Intro;
