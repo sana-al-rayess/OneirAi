11678a1cba632740fdda75c5e6c7ccf131fceb1f
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 13:07:31 +0300

    edited header component

--- oneirai-frontend/src/components/Header/index.jsx
+++ oneirai-frontend/src/components/Header/index.jsx
@@ -4,7 +4,15 @@ import Logo from "../../images/logo_oneira.png";
 
 const Header = () => {
   return (
-   
+    <div className='header1'>
+    <div>
+      <img className='logo' src={Logo} alt='logo' />
+    </div>
+    <div className="button-container">
+      <button className='login-button'>Login</button>
+      <button className='signup-button'>SignUp</button>
+    </div>
+  </div>
   );
 };
 
