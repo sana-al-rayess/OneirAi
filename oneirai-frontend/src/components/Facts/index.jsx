b001982dff8e978e06c2d5def6d124f9b3f85285
Sana Rayess <rayess.sana@gmail.com>
2023-04-15 23:47:20 +0300

    added card to fact component

--- oneirai-frontend/src/components/Facts/index.jsx
+++ oneirai-frontend/src/components/Facts/index.jsx
@@ -4,7 +4,17 @@ const Facts = () => {
 
     return (
         <div>
-            
+            <p className="feature-title">Did You Know?</p>
+            <div class="container-cards">
+                <div class="card">
+                    <h3 class="title">Fact 1</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+
+                </div>
+                </div>
         </div>
 
     );
