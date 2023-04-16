f23e72c53976eaa75f083ed88a0a3cf605056da5
Sana Rayess <rayess.sana@gmail.com>
2023-04-16 17:28:17 +0300

    added logo to side-nav component

--- oneirai-frontend/src/components/SideNav/index.jsx
+++ oneirai-frontend/src/components/SideNav/index.jsx
@@ -17,44 +17,7 @@ const SideNav = () => {
 
       <div> <img className="logo" src={Logo} alt="logo" /></div>
 
-      <div className="personal-info">
-        <div> <img src={PP1} alt="Profile Image" className="profile-pic"></img> </div>
-        <div><p>John Doe</p></div>
-        <div><a>Change Password</a></div>
-      </div>
-      <br /><br />
-      <div className="menu">
-        <div>
-          <div className="menu-item">
-            <img src={Journal} alt="icon" className="menu-icon"></img>
-            <Link to="/user" className="linking">
-              Dream Journal
-            </Link>
-          </div>
-          <br />
-          <div>
-            <div className="menu-item">
-              <img src={Pattern} alt="icon" className="menu-icon"></img>
-              <Link to="/pattern" className="linking">
-                Pattern Tracker
-              </Link>
-            </div>
-            <br />
-            <div>
-              <div className="menu-item">
-                <img src={Edit} alt="icon" className="menu-icon"></img>
-                <Link to="/profile" className="linking">
-                  Profile
-                </Link>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-        <div><Button>Logout</Button></div>
-
-      </div>
+      
 
     </div>
   );
