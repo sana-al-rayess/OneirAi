88570c97421977e0e4bb25cddf03d562bbcd2cd6
Sana Rayess <rayess.sana@gmail.com>
2023-04-16 17:30:40 +0300

    added menu items with links to side nav component

--- oneirai-frontend/src/components/SideNav/index.jsx
+++ oneirai-frontend/src/components/SideNav/index.jsx
@@ -22,7 +22,40 @@ const SideNav = () => {
         <div><p>John Doe</p></div>
         <div><a>Change Password</a></div>
       </div>
-      
+      <br /><br />
+      <div className="menu">
+        <div>
+          <div className="menu-item">
+            <img src={Journal} alt="icon" className="menu-icon"></img>
+            <Link to="/user" className="linking">
+              Dream Journal
+            </Link>
+          </div>
+          <br />
+          <div>
+            <div className="menu-item">
+              <img src={Pattern} alt="icon" className="menu-icon"></img>
+              <Link to="/pattern" className="linking">
+                Pattern Tracker
+              </Link>
+            </div>
+            <br />
+            <div>
+              <div className="menu-item">
+                <img src={Edit} alt="icon" className="menu-icon"></img>
+                <Link to="/profile" className="linking">
+                  Profile
+                </Link>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+        <div><Button>Logout</Button></div>
+
+      </div>
+
     </div>
   );
 }
