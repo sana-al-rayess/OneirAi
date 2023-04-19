50a2cac41f938f2fafe83081d7cf60790f71c424
Sana Rayess <rayess.sana@gmail.com>
2023-04-19 10:57:49 +0300

    edited register routes

--- oneirai-backend/routes/api.php
+++ oneirai-backend/routes/api.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::post('refresh', 'refresh');
 
-});
+
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('logout', [UserController::class, 'logout']);
+
