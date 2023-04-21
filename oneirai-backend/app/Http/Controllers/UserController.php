9f406db83fe396f456ff6642281b25258f24d506
Sana Rayess <rayess.sana@gmail.com>
2023-04-21 15:48:25 +0300

    edited change password api

--- oneirai-backend/app/Http/Controllers/UserController.php
+++ oneirai-backend/app/Http/Controllers/UserController.php
@@ -125,40 +125,38 @@ class UserController extends Controller
     }
 
     public function updatePassword(Request $request)
-    {
-        $user_id = Auth::id();
-        $password_updated = User::find($user_id);
-    
-        $validator = Validator::make($request->all(), [
-            'current_password' => 'required',
-            'password' => 'required|min:8|confirmed',
-        ]);
-    
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 'error',
-                'errors' => $validator->errors()
-            ], 422);
-        }
-    
-        $current_password = $request->input('current_password');
-    
-        if (!Hash::check($current_password, $password_updated->password)) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'The current password is incorrect.'
-            ], 401);
-        }
-    
-        $password_updated->password = Hash::make($request->input('password'));
-        $password_updated->save();
-    
+{
+    $user_id = Auth::id();
+    $password_updated = User::find($user_id);
+
+    $validator = Validator::make($request->all(), [
+        'current_password' => 'required',
+        'password' => 'required|min:8|confirmed',
+        'password_confirmation' => 'required|same:password',
+    ]);
+
+    if ($validator->fails()) {
         return response()->json([
-            "status" => "success",
-            "updated_user" => $password_updated
-        ], 200);
+            'status' => 'error',
+            'errors' => $validator->errors()
+        ], 422);
     }
-    
+
+    $current_password = $request->input('current_password');
+
+    if (!Hash::check($current_password, $password_updated->password)) {
+        return response()->json(['error' => 'The current password is incorrect.'], 422);
+    }
+
+    $password_updated->password = Hash::make($request->input('password'));
+    $password_updated->save();
+
+    return response()->json([
+        "status" => "success",
+        "updated_user" => $password_updated
+    ], 200);
+}
+
     
     
     
