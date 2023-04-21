c4fba4d03d8c08a4a3e0e5260fdfa8e775dde9cd
Sana Rayess <rayess.sana@gmail.com>
2023-04-21 22:11:48 +0300

    created an api to add a dream

--- oneirai-backend/app/Http/Controllers/DreamController.php
+++ oneirai-backend/app/Http/Controllers/DreamController.php
@@ -9,6 +9,26 @@ use App\Models\Dream;
 
 class DreamController extends Controller
 {
+    public function addDream(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'date' => 'required'
+        ]);
     
+        $dream = Dream::create([
+            'user_id' => Auth::id(),
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'date' => $request->input('date'),
+        ]);
+    
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Dream saved successfully',
+            'dream' => $dream,
+        ]);
+    }
     
 }
