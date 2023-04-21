065635e1f9d4b6773fe4bd823eaf41748f60771b
Sana Rayess <rayess.sana@gmail.com>
2023-04-21 22:11:09 +0300

    created a dream controller

---
+++ oneirai-backend/app/Http/Controllers/DreamController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Dream;
+
+class DreamController extends Controller
+{
+    
+    
+}
