68c1c2b6d953ad9598b9c07803d11fdc3bb8f548
Sana Rayess <rayess.sana@gmail.com>
2023-04-21 09:48:46 +0300

    added route to update password

--- oneirai-backend/routes/api.php
+++ oneirai-backend/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\UserController;
 
 
 
+
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 Route::post('logout', [UserController::class, 'logout']);
@@ -15,5 +16,5 @@ Route::post('logout', [UserController::class, 'logout']);
 Route::group(['middleware' => 'user.role'], function () {
     Route::get('userInfo', [UserController::class, 'getUser']);
     Route::post('updateUserInfo', [UserController::class, 'updateUser']);
-
+    Route::post('updatePassword', [UserController::class, 'updatePassword']);
 });
