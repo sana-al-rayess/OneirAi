5adf4d0914f71bc4a05bdd30240093cf7cd6a09e
Sana Rayess <rayess.sana@gmail.com>
2023-04-21 23:23:22 +0300

    created an api to get all dreams of a user

--- oneirai-backend/app/Http/Controllers/DreamController.php
+++ oneirai-backend/app/Http/Controllers/DreamController.php
@@ -16,44 +16,54 @@ class DreamController extends Controller
             'description' => 'required|string',
             'date' => 'required'
         ]);
-    
+
         $dream = Dream::create([
             'user_id' => Auth::id(),
             'title' => $request->input('title'),
             'description' => $request->input('description'),
             'date' => $request->input('date'),
         ]);
-    
+
         return response()->json([
             'status' => 'success',
             'message' => 'Dream saved successfully',
             'dream' => $dream,
         ]);
     }
-    
+
     public function deleteDream(Request $request, $id)
+    {
+        $dream = Dream::find($id);
+
+        if (!$dream) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Dream not found',
+            ], 404);
+        }
+
+        if ($dream->user_id != Auth::id()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $dream->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Dream deleted successfully',
+        ]);
+    }
+
+    public function getDreams()
 {
-    $dream = Dream::find($id);
-
-    if (!$dream) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Dream not found',
-        ], 404);
-    }
-
-    if ($dream->user_id != Auth::id()) {
-        return response()->json([
-            'status' => 'error',
-            'message' => 'Unauthorized',
-        ], 401);
-    }
-
-    $dream->delete();
+    $dreams = Dream::where('user_id', Auth::id())->get();
 
     return response()->json([
         'status' => 'success',
-        'message' => 'Dream deleted successfully',
+        'data' => $dreams,
     ]);
 }
 
