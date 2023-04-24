1bda22409e6eac761c1d9ffa7d94170b24ff8d58
Sana Rayess <rayess.sana@gmail.com>
2023-04-24 16:54:08 +0300

    added a button to error page

--- oneirai-frontend/src/components/ErrorPage/index.jsx
+++ oneirai-frontend/src/components/ErrorPage/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./errorpage.css";
-
+import Button2 from "../Button2";
+import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
     <div className="error-container">
       <h1 className="error-h1">404 Error: Page not found</h1>
       <p className="error-p">You are not authorized to access this page.</p>
-      
+      <Link to="/login">
+        <Button2 className="back-button">Go Back</Button2>
+      </Link>
     </div>
   );
 }
