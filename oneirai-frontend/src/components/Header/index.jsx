6fe8161faf285383b5be2413f45d83c7bfe362a6
Sana Rayess <rayess.sana@gmail.com>
2023-04-24 14:31:09 +0300

    added onclick even to sign up

--- oneirai-frontend/src/components/Header/index.jsx
+++ oneirai-frontend/src/components/Header/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 import "./header.css";
 import Logo from "../../images/logo_oneira.png";
 import Button from "../../components/Button";
 
+
 const Header = () => {
+
+  const [clicked, setClicked] = useState(false);
+  const active_user = localStorage.getItem("user_id");
+  const handleClick = () => {
+    window.location.href = "/login";
+    localStorage.clear();
+  };
+
+ 
+  const handleRoute = (route) => {
+    return active_user ? route : "/reg";
+  };
+
   return (
     <div className='header1'>
-    <div>
-      <img className='logo' src={Logo} alt='logo' />
+      <div>
+        <img className='logo' src={Logo} alt='logo' />
+      </div>
+      <div className="button-container">
+       
+
+        <Button onClick={handleClick}>{localStorage.getItem("user_id") ? "Log Out" : "Sign In "}</Button>
+      </div>
     </div>
-    <div className="button-container">
-      <button className='login-button'>Login</button>
-      <Button>SignUp</Button>
-    </div>
-  </div>
   );
 };
 
