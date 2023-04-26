6456646f3e8ee4e04a745db8d08c29479f110705
Sana Rayess <rayess.sana@gmail.com>
2023-04-26 11:32:47 +0300

    frontend: imported error page

--- oneirai-frontend/src/App.js
+++ oneirai-frontend/src/App.js
@@ -6,22 +6,27 @@ import Landing from "./Pages/Landing";
 import UserDashboard from "./Pages/UserDashboard";
 import Profile from "./Pages/Profile";
 import Patterns from "./Pages/Patterns";
-import Admin from "./Pages/Admin";
+
+import ErrorPage from "./components/ErrorPage";
+
+
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-     
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="/user" element={<UserDashboard />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/pattern" element={<Patterns />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="*" element={<div>404</div>} />
-    </Routes>
-  </BrowserRouter>
+      <Routes>
+        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="/user" element={<UserDashboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/pattern" element={<Patterns />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="*" element={<div>404</div>} />
+        
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
