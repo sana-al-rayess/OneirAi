c901596a68760f651d3ac9aab2b14b367f447cf6
Sana Rayess <rayess.sana@gmail.com>
2023-04-26 11:25:19 +0300

    frontend: added validations to addDream modal

--- oneirai-frontend/src/components/AddDream/index.jsx
+++ oneirai-frontend/src/components/AddDream/index.jsx
@@ -2,23 +2,25 @@ import React, { useState } from "react";
 import Button3 from "../Button3";
 import "./adddream.css";
 import axios from "axios";
-
-
 const AddDream = (props) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [date, setDate] = useState("");
+  const [errors, setErrors] = useState({});
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
+    setErrors({});
   };
 
   const handleDescriptionChange = (e) => {
     setDescription(e.target.value);
+    setErrors({});
   };
 
   const handleDateChange = (e) => {
     setDate(e.target.value);
+    setErrors({});
   };
 
   const handleSubmit = (e) => {
@@ -29,6 +31,22 @@ const AddDream = (props) => {
     formData.append('title', title);
     formData.append('description', description);
     formData.append('date', date);
+
+   
+    const errors = {};
+    if (!title) {
+      errors.title = "Title is required";
+    }
+    if (!description) {
+      errors.description = "Description is required";
+    }
+    if (!date) {
+      errors.date = "Date is required";
+    }
+    if (Object.keys(errors).length > 0) {
+      setErrors(errors);
+      return;
+    }
 
     axios
       .post("http://127.0.0.1:8000/api/addDream", formData, {
@@ -48,8 +66,6 @@ const AddDream = (props) => {
       });
   };
 
-
-
   return (
     <div className="popup">
       <div className="popup-inner">
@@ -66,6 +82,7 @@ const AddDream = (props) => {
               value={title}
               onChange={handleTitleChange}
             />
+            {errors.title && <span className="error-dream">{errors.title}</span>}
           </div>
           <div className="labeltext">
             <label htmlFor="description">Description:</label>
@@ -75,6 +92,7 @@ const AddDream = (props) => {
               value={description}
               onChange={handleDescriptionChange}
             ></textarea>
+            {errors.description && <span className="error-dream">{errors.description}</span>}
           </div>
           <div className="labeltext">
             <label htmlFor="date">Date:</label>
@@ -85,7 +103,9 @@ const AddDream = (props) => {
               value={date}
               onChange={handleDateChange}
             />
+            {errors.date && <span className="error-dream">{errors.date}</span>}
           </div>
+
           <div className="model-btn">
             <Button3 onClick={props.closePopup}>Close</Button3>
             <Button3 type="submit">Save</Button3>
