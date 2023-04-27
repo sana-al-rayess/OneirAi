a188258b12e610f7bae8f9e21c53af7a40775825
Sana Rayess <rayess.sana@gmail.com>
2023-04-27 20:04:18 +0300

    frontend: created horoscope page

---
+++ oneirai-frontend/src/Pages/Horoscope/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ParticlesBg from "particles-bg";
+import './horoscope.css';
+
+
+const Horoscope = () => {
+  return (
+    <div>
+      <ParticlesBg type="cobweb" bg={true} color="#ffffff" num={50} shape="star" />
+      
+   
+    </div>
+  );
+};
+
+export default Horoscope;
