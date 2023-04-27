9411b205819c683810dba64a3bc0c6b690ee7103
Sana Rayess <rayess.sana@gmail.com>
2023-04-27 20:11:20 +0300

    frontend:added DailyHoro comp to Horoscope page

--- oneirai-frontend/src/Pages/Horoscope/index.jsx
+++ oneirai-frontend/src/Pages/Horoscope/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ParticlesBg from "particles-bg";
 import './horoscope.css';
 import Navbar from "../../components/NavBar";
-
+import DailyHoroscope from "../../components/DailyHoroscope";
 
 
 const Horoscope = () => {
@@ -10,7 +10,7 @@ const Horoscope = () => {
     <div>
       <ParticlesBg type="cobweb" bg={true} color="#ffffff" num={50} shape="star" />
       <Navbar className="navbar-horo" />
-      
+      <DailyHoroscope />
    
     </div>
   );
