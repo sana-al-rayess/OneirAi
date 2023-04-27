cc2cb3d564cc80bcb5c6d665676b08b2ce8f9985
Sana Rayess <rayess.sana@gmail.com>
2023-04-27 19:53:23 +0300

    backend: created  api that uses ai to getHoroscope

--- oneirai-backend/app/Http/Controllers/HoroscopeController.php
+++ oneirai-backend/app/Http/Controllers/HoroscopeController.php
@@ -10,5 +10,22 @@ use GuzzleHttp\Client;
 
 class HoroscopeController extends Controller
 {
-  
+    public function getHoroscope(Request $request)
+    {
+        $client = new Client(['base_uri' => 'https://api.openai.com']);
+        $response = $client->request('POST', '/v1/engines/text-davinci-002/completions', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'Authorization' => 'Bearer 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',
+            ],
+            'json' => [
+                'prompt' => 'Daily Horoscope for ' . $request->input('sign'),
+                'max_tokens' => 256,
+                'temperature' => 0.7,
+            ],
+        ]);
+
+        $horoscope = json_decode($response->getBody(), true)['choices'][0]['text'];
+        return response()->json(['horoscope' => $horoscope]);
+    }
 }
