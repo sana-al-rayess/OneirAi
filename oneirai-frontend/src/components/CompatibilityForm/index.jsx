5cb3ee1d76fc91be12d69605abcc0408f3806f6d
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:34:04 +0300

    frontend:created CompatibilityForm component

---
+++ oneirai-frontend/src/components/CompatibilityForm/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import "./compatibilityform.css";
+import Button2 from '../Button2';
+import axios from 'axios';
+
+function CompatibilityForm() {
+   
+    
+    return (
+        <div></div>
+
+}
+
+export default CompatibilityForm;
+
