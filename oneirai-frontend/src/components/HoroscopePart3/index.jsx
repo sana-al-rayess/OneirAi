1b12976124133f148adc07d23254d48eb612e9b6
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:44:07 +0300

    frontend: added h1 and img to HoroscopePart3

--- oneirai-frontend/src/components/HoroscopePart3/index.jsx
+++ oneirai-frontend/src/components/HoroscopePart3/index.jsx
@@ -10,7 +10,17 @@ function HoroscopePart3() {
     return (
         <div>
 
-            
+            <div className="horoscope-part3">
+                <ParticlesBg type="cobweb" bg={true} color="#ffffff" num={50} shape="star" />
+
+                <div><h1 className="horo-slogan2">
+                    <div>Discover the stars' perfect match<br />  with AI-powered Zodiac-<br />Compatibility Test.
+                    </div></h1>
+        
+                    <div> <CompatibilityForm /></div>
+                </div>
+                <div className="comp"><img className="comp-img" src={Comp3} alt="photo" /></div>
+            </div >
         </div>
     );
 }
