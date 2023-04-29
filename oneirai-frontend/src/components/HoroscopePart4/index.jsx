fca3a486545887b1ff0ad796bcced50aac1a5358
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:46:29 +0300

    frontend: created HoroscopePart4 component

---
+++ oneirai-frontend/src/components/HoroscopePart4/index.jsx
@@ -0,0 +1,17 @@
+import Per2 from "../../images/per2.png"
+import Button2 from "../Button2";
+import ParticlesBg from "particles-bg";
+import CompatibilityForm from "../CompatibilityForm";
+import Personality from "../Personality";
+
+function HoroscopePart4() {
+
+    return (
+        <div>
+
+           
+        </div>
+    );
+}
+
+export default HoroscopePart4;
