edc5e613c6164f89a7168a7dc6c3ea00973431af
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:51:57 +0300

    frontend: created Personality component

---
+++ oneirai-frontend/src/components/Personality/index.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import Button2 from '../Button2';
+import axios from 'axios';
+import './personality.css';
+
+function Personality() {
+ 
+
+  const handleSubmit = async (event) => {
+   
+  };
+
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default Personality;
