70df6aaf28decfb968ee17225f628e02f0fd6dfd
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 22:29:22 +0300

    frontend: created HoroscopePart1 component

---
+++ oneirai-frontend/src/components/HoroscopePart1/index.jsx
@@ -0,0 +1,18 @@
+import "./horoscopepart1.css";
+import Zodiac1 from "../../images/zo1.png"
+import Zodiac2 from "../../images/z03.png"
+import Button2 from "../Button2";
+import DailyHoroscope from "../DailyHoroscope";
+function HoroscopePart1() {
+
+    return (
+        <div className="horoscope-part1">
+
+
+            
+        </div>
+
+    );
+}
+
+export default HoroscopePart1;
