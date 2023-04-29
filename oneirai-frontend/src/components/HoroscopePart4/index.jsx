cd67bbc8bb93a0eb4208cd49559c82f066b2c205
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:48:23 +0300

    frontend: added h1 and img to HoroscopePart4

--- oneirai-frontend/src/components/HoroscopePart4/index.jsx
+++ oneirai-frontend/src/components/HoroscopePart4/index.jsx
@@ -9,7 +9,15 @@ function HoroscopePart4() {
     return (
         <div>
 
-           
+            <div className="horoscope-part3">
+                <ParticlesBg type="cobweb" bg={true} color="#ffffff" num={50} shape="star" />
+                <div className="comp"><img className="comp-img" src={Per2} alt="photo" /></div>
+                <div><h1 className="horo-slogan2"><div>Unlock the Secrets<br /> of Your True Self:<br />Discover Your Unique Personality.
+                </div></h1>
+                <div> <Personality /></div>
+                </div>
+
+            </div >
         </div>
     );
 }
