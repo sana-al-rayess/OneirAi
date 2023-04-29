33aaf10129bcf4cd80f0032b237a7d6544db9f02
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:43:06 +0300

    frontend: created HoroscopePart3 component

---
+++ oneirai-frontend/src/components/HoroscopePart3/index.jsx
@@ -0,0 +1,18 @@
+import "./horoscopepart3.css";
+import Comp from "../../images/compatibility.png"
+import Comp3 from "../../images/comp3.png"
+import Button2 from "../Button2";
+import ParticlesBg from "particles-bg";
+import CompatibilityForm from "../CompatibilityForm";
+
+function HoroscopePart3() {
+
+    return (
+        <div>
+
+            
+        </div>
+    );
+}
+
+export default HoroscopePart3;
