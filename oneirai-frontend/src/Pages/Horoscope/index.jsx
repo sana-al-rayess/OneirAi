f8360971d19dd89cfde6a374a53cec4b5bf67070
Sana Rayess <rayess.sana@gmail.com>
2023-04-30 00:12:46 +0300

    frontend: edited Horoscope page

--- oneirai-frontend/src/Pages/Horoscope/index.jsx
+++ oneirai-frontend/src/Pages/Horoscope/index.jsx
@@ -3,14 +3,24 @@ import ParticlesBg from "particles-bg";
 import './horoscope.css';
 import Navbar from "../../components/NavBar";
 import DailyHoroscope from "../../components/DailyHoroscope";
-
+import HoroscopePart1 from "../../components/HoroscopePart1";
+import HoroscopePart2 from "../../components/HoroscopePart2";
+import HoroscopePart3 from "../../components/HoroscopePart3";
+import HoroscopePart4 from "../../components/HoroscopePart4";
+import Footer from "../../components/Footer";
 
 const Horoscope = () => {
   return (
-    <div>
+    <div className="body-horo">
       <ParticlesBg type="cobweb" bg={true} color="#ffffff" num={50} shape="star" />
       <Navbar className="navbar-horo" />
-      <DailyHoroscope />
+      <HoroscopePart1 />
+      {/* <HoroscopePart2 /> */}
+      <HoroscopePart4 />
+      <HoroscopePart3 />
+    
+      <Footer />
+      
    
     </div>
   );
