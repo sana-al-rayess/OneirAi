8874eca2e32d265ace9a051d0f08a9fffe78c5b4
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:52:34 +0300

    frontend: added useStates to Personality

--- oneirai-frontend/src/components/Personality/index.jsx
+++ oneirai-frontend/src/components/Personality/index.jsx
@@ -4,7 +4,11 @@ import axios from 'axios';
 import './personality.css';
 
 function Personality() {
- 
+  const [userBirthday, setUserBirthday] = useState('');
+  const [compatibilityScore, setCompatibilityScore] = useState(null);
+  const [errorMsg, setErrorMsg] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [tableHtml, setTableHtml] = useState('');
 
   const handleSubmit = async (event) => {
    
