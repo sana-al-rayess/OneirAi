7f9559e7fce8f42f6505a88007b7d5f672f84e8b
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:35:23 +0300

    frontend:added useSates to CompatibilityForm

--- oneirai-frontend/src/components/CompatibilityForm/index.jsx
+++ oneirai-frontend/src/components/CompatibilityForm/index.jsx
@@ -4,10 +4,15 @@ import Button2 from '../Button2';
 import axios from 'axios';
 
 function CompatibilityForm() {
-   
+    const [userBirthday, setUserBirthday] = useState('');
+    const [partnerBirthday, setPartnerBirthday] = useState('');
+    const [compatibilityScore, setCompatibilityScore] = useState(null);
+    const [errorMsg, setErrorMsg] = useState('');
+
+    
     
     return (
-        <div></div>
+        <div>
 
 }
 
