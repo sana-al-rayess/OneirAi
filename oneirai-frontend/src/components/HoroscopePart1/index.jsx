0bbd217dba273196fb8c0830dc2398096a0e9cc1
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 22:30:46 +0300

    frontend: created a title for Horoscope Page

--- oneirai-frontend/src/components/HoroscopePart1/index.jsx
+++ oneirai-frontend/src/components/HoroscopePart1/index.jsx
@@ -9,6 +9,50 @@ function HoroscopePart1() {
         <div className="horoscope-part1">
 
 
+            <div class="overlay"></div>
+
+            <div class="text">
+                <div class="wrapper">
+                    <div id="L" class="letter">H</div>
+                    <div class="shadow">H</div>
+                </div>
+                <div class="wrapper">
+                    <div id="I" class="letter">O</div>
+                    <div class="shadow">O</div>
+                </div>
+                <div class="wrapper">
+                    <div id="G" class="letter">R</div>
+                    <div class="shadow">R</div>
+                </div>
+                <div class="wrapper">
+                    <div id="H" class="letter">O</div>
+                    <div class="shadow">O</div>
+                </div>
+                <div class="wrapper">
+                    <div id="T" class="letter">S</div>
+                    <div class="shadow">S</div>
+                </div>
+                <div class="wrapper">
+                    <div id="N" class="letter">C</div>
+                    <div class="shadow">C</div>
+                </div>
+                <div class="wrapper">
+                    <div id="E" class="letter">O</div>
+                    <div class="shadow">O</div>
+                </div>
+                <div class="wrapper">
+                    <div id="S" class="letter">P</div>
+                    <div class="shadow">P</div>
+                </div>
+                <div class="wrapper">
+                    <div id="Stwo" class="letter">E</div>
+                    <div class="shadow">E</div>
+                </div>
+            </div>
+
+
+
+          
             
         </div>
 
