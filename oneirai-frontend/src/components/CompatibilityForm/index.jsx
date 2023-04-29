a387e87ad485ae369f4fe1c1b595d8f40ca02692
Sana Rayess <rayess.sana@gmail.com>
2023-04-29 23:37:08 +0300

    frontend:created form: CompatibilityForm component

--- oneirai-frontend/src/components/CompatibilityForm/index.jsx
+++ oneirai-frontend/src/components/CompatibilityForm/index.jsx
@@ -9,11 +9,49 @@ function CompatibilityForm() {
     const [compatibilityScore, setCompatibilityScore] = useState(null);
     const [errorMsg, setErrorMsg] = useState('');
 
-    
+    const handleSubmit = (e) => {
     
     return (
         <div>
 
+            <form className='form-horo' onSubmit={handleSubmit}>
+
+                <div className='horo-field'>
+                    <div><label htmlFor="user-birthday" className='horo-label'>Your Birthdate:</label></div>
+                    <div><label htmlFor="partner-birthday" className='horo-label'>Partner's Birthdate:</label></div>
+                </div>
+
+                <br />
+
+                <div className='horo-field'>
+                    <div>
+                        <input
+                            className='date-horo2'
+                            id='user-birthday'
+                            type="date"
+                            value={userBirthday}
+                            onChange={(e) => setUserBirthday(e.target.value)}
+                        />
+                    </div>
+
+                    <div className='input-btn'>
+                        <input
+                            className='date-horo2'
+                            id='partner-birthday'
+                            type="date"
+                            value={partnerBirthday}
+                            onChange={(e) => setPartnerBirthday(e.target.value)}
+                        />
+                    </div>
+
+                    <div className='btn-horo'>
+                        <Button2 type="submit">Go</Button2>
+                    </div>
+                </div>
+            </form>
+          
+        </div>
+    );
 }
 
 export default CompatibilityForm;
