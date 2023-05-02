0b27eef4f4f1703d82ccb48af5d18dd5fbd62f96
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 22:55:34 +0300

    backend: edite mail.php in config

--- oneirai-backend/config/mail.php
+++ oneirai-backend/config/mail.php
@@ -33,10 +33,11 @@ return [
     |
     */
 
+    
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME'),
