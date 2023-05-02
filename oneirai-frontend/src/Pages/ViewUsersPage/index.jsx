1d20b418d3365f97d2535f8d8118ffe1a532c4a1
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 09:20:26 +0300

    frontend: added components to ViewUsersPage

--- oneirai-frontend/src/Pages/ViewUsersPage/index.jsx
+++ oneirai-frontend/src/Pages/ViewUsersPage/index.jsx
@@ -1,11 +1,21 @@
+import UserTable from "../../components/UserTable";
+import AdminNav from "../../components/AdminNav";
+
+
 
 const ViewUsersPage = () => {
     return (
         <div className="body3">
-    
+            <div className="box-admin">
+                <div>
+                    <AdminNav />
+                </div>
+                <div className="admin-left">
+                    <UserTable/>
+                </div>
+            </div>
         </div>
        
-
     );
 };
 
