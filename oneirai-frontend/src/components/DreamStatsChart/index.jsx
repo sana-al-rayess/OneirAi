7c84d3d37677f65d8d4683a366f5fa5d016c7c65
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 08:58:51 +0300

    frontend: added divs to display pie charts

--- oneirai-frontend/src/components/DreamStatsChart/index.jsx
+++ oneirai-frontend/src/components/DreamStatsChart/index.jsx
@@ -66,7 +66,18 @@ const DreamStatsChart = () => {
   }, [data]);
 
   return (
-  
- 
+    <div className='body-stats'>
+      <h1>Pie Charts of User's Locations</h1>
+      <div className='charts-container'>
+        {data &&
+          data.stats.map((locationData) => (
+            <div key={locationData.location} className='chart-container'>
+              <h3>{locationData.location}</h3>
+              <canvas id={`locationChart-${locationData.location}`}></canvas>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 export default DreamStatsChart;
