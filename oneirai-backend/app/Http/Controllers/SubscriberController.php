a8282c04e28b7d99ac9b975aec40227708e0663b
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 22:50:23 +0300

    backend: created SubscriberComponent

---
+++ oneirai-backend/app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Mail\Subscribe;
+use App\Models\Subscriber;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class SubscriberController extends Controller
+{
+    
+
+
+    
+}
