ebce9bade51b67dcc00626acf800e3ba57e2f5d4
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 22:51:36 +0300

    backend: created subscribe api

--- oneirai-backend/app/Http/Controllers/SubscriberController.php
+++ oneirai-backend/app/Http/Controllers/SubscriberController.php
@@ -13,7 +13,35 @@ use Illuminate\Support\Facades\Validator;
 class SubscriberController extends Controller
 {
     
-
+    public function subscribe(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:subscribers'
+        ]);
+        if ($validator->fails()) {
+            return new JsonResponse(
+                [
+                    'success' => false,
+                    'message' => $validator->errors()
+                ],
+                422
+            );
+        }
+        $email = $request->all()['email'];
+        $subscriber = Subscriber::create([
+            'email' => $email
+        ]);
+        if ($subscriber) {
+            Mail::to($email)->send(new Subscribe($email));
+            return new JsonResponse(
+                [
+                    'success' => true,
+                    'message' => "Thank you for subscribing to our email, please check your inbox"
+                ],
+                200
+            );
+        }
+    }
 
     
 }
