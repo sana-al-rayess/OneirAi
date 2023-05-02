0886bfb091358e39b770fd53b41e7a22afe23d7f
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 22:59:07 +0300

    frontend: added ErrorPage comp to ViewUsersPage

--- oneirai-frontend/src/Pages/ViewUsersPage/index.jsx
+++ oneirai-frontend/src/Pages/ViewUsersPage/index.jsx
@@ -1,11 +1,14 @@
 import UserTable from "../../components/UserTable";
 import AdminNav from "../../components/AdminNav";
+import ErrorPage from "../../components/ErrorPage";
 
 
 
 const ViewUsersPage = () => {
+    const type = localStorage.getItem("type");
     return (
         <div className="body3">
+             {type === "admin" ? (
             <div className="box-admin">
                 <div>
                     <AdminNav />
@@ -14,6 +17,9 @@ const ViewUsersPage = () => {
                     <UserTable/>
                 </div>
             </div>
+             ) : (
+                <ErrorPage />
+              )}
         </div>
        
     );
