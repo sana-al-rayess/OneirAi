969bf11a3b54edc2b59464e2a92482c79365d31b
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 09:14:11 +0300

    frontend:added AdminNav,LineChart to LineChartPage

--- oneirai-frontend/src/Pages/LineChartPage/index.jsx
+++ oneirai-frontend/src/Pages/LineChartPage/index.jsx
@@ -1,8 +1,24 @@
+import React, { useState } from "react";
+import LineChart from "../../components/LineChart";
+import AdminNav from "../../components/AdminNav";
+
 
 
 const LineChartPage = () => {
     return (
-    )
+        <div className="body3">
+            <div className="box-admin">
+                <div>
+                    <AdminNav />
+                </div>
+                <div className="admin-left">
+                    <LineChart />
+                </div>
+            </div>
+        </div>
+       
+
+    );
 };
 
 export default LineChartPage;
