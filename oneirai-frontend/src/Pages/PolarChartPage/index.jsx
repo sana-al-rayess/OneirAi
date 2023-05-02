abe5bd945d0f8d61d7baa20ab07cd6986705c478
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 09:18:18 +0300

    frontend: added components to PolarChartPage

--- oneirai-frontend/src/Pages/PolarChartPage/index.jsx
+++ oneirai-frontend/src/Pages/PolarChartPage/index.jsx
@@ -1,9 +1,19 @@
+import AdminNav from "../../components/AdminNav";
+import PolarAreaChart from "../../components/PolarAreaChart";
+
 
 
 const PolarChartPage = () => {
     return (
         <div className="body3">
-       
+            <div className="box-admin">
+                <div>
+                    <AdminNav />
+                </div>
+                <div className="admin-left">
+                    <PolarAreaChart />
+                </div>
+            </div>
         </div>
        
 
