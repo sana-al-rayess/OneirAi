31e0c5ef0b35f8014d5c9343a69bbd21a7f86a52
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 09:17:01 +0300

    frontend:added AdminNav,DreamStatsChart to page

--- oneirai-frontend/src/Pages/PieChartPage/index.jsx
+++ oneirai-frontend/src/Pages/PieChartPage/index.jsx
@@ -1,8 +1,19 @@
+import DreamStatsChart from "../../components/DreamStatsChart";
+import AdminNav from "../../components/AdminNav";
+
+
 
 const PieChartPage = () => {
     return (
         <div className="body3">
-        
+            <div className="box-admin">
+                <div>
+                    <AdminNav />
+                </div>
+                <div className="admin-left">
+                    <DreamStatsChart />
+                </div>
+            </div>
         </div>
        
 
