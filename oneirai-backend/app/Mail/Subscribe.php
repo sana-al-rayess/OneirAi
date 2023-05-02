dfb3992f61f5306ff10128c8f1e588432e16b2ec
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 22:53:51 +0300

    backend: added subscribe.php to app/mail

---
+++ oneirai-backend/app/Mail/Subscribe.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Mail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+
+
+class Subscribe extends Mailable
+{
+
+    
+    use Queueable, SerializesModels;
+    public $email;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($email)
+    {
+        $this->email = $email;
+    }
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Thank you for subscribing to our newsletter')
+        ->markdown('emails.subscribers');
+    }
+}
