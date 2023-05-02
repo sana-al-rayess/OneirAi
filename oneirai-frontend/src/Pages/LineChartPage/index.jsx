ec814241d0e577c802761d61e17bf02e6a3af3d0
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 23:02:52 +0300

    frontend: added ErrorPage Comp to LineChartPage

--- oneirai-frontend/src/Pages/LineChartPage/index.jsx
+++ oneirai-frontend/src/Pages/LineChartPage/index.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import LineChart from "../../components/LineChart";
 import AdminNav from "../../components/AdminNav";
+import ErrorPage from "../../components/ErrorPage";
 
 
 
 const LineChartPage = () => {
+    const type = localStorage.getItem("type");
     return (
         <div className="body3">
+            {type === "admin" ? (
             <div className="box-admin">
                 <div>
                     <AdminNav />
@@ -15,6 +18,9 @@ const LineChartPage = () => {
                     <LineChart />
                 </div>
             </div>
+              ) : (
+                <ErrorPage />
+              )}
         </div>
        
 
