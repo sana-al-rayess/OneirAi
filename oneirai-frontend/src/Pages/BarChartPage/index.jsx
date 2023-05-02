7f656d01932a7d9d004bece6213b3a23eece7bae
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 09:12:19 +0300

    frontend:added AdminNav, BarChart to BarChartPage

--- oneirai-frontend/src/Pages/BarChartPage/index.jsx
+++ oneirai-frontend/src/Pages/BarChartPage/index.jsx
@@ -1,9 +1,21 @@
+import BarChart from "../../components/BarChart";
+import AdminNav from "../../components/AdminNav";
 
 
 
 const BarChartPage = () => {
     return (
-   
+        <div className="body3">
+            <div className="box-admin">
+                <div>
+                    <AdminNav />
+                </div>
+                <div className="admin-left">
+                    <BarChart />
+                </div>
+            </div>
+        </div>
+       
 
     );
 };
