aa1d19a92d46d07327288fe36c1c36a67a95f45f
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 23:01:39 +0300

    frontend: added ErrorPage comp to BarChartPage

--- oneirai-frontend/src/Pages/BarChartPage/index.jsx
+++ oneirai-frontend/src/Pages/BarChartPage/index.jsx
@@ -1,11 +1,13 @@
 import BarChart from "../../components/BarChart";
 import AdminNav from "../../components/AdminNav";
-
+import ErrorPage from "../../components/ErrorPage";
 
 
 const BarChartPage = () => {
+    const type = localStorage.getItem("type");
     return (
         <div className="body3">
+            {type === "admin" ? (
             <div className="box-admin">
                 <div>
                     <AdminNav />
@@ -14,6 +16,9 @@ const BarChartPage = () => {
                     <BarChart />
                 </div>
             </div>
+            ) : (
+                <ErrorPage />
+              )}
         </div>
        
 
