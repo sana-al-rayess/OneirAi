716814d1b47abb6519b6e339ac4d0876119d2373
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 08:40:59 +0300

    frontend: created a table in UserTable component

--- oneirai-frontend/src/components/UserTable/index.jsx
+++ oneirai-frontend/src/components/UserTable/index.jsx
@@ -1,7 +1,34 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './UserTable.css'; // import CSS file
 
 function UserTable() {
 
+
+  return (
+    <table className="user-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Age</th>
+          <th>Location</th>
+          
+        </tr>
+      </thead>
+      <tbody>
+        {users.map(user => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.Age}</td>
+            <td>{user.location}</td>
+          
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
       
 export default UserTable;
