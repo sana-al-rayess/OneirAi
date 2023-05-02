8c0cf1aff95cd7c4a70c8400b2418294e49fe9e9
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 08:58:09 +0300

    frontend: created a fn to display  pie charts

--- oneirai-frontend/src/components/DreamStatsChart/index.jsx
+++ oneirai-frontend/src/components/DreamStatsChart/index.jsx
@@ -1,7 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Chart from 'chart.js/auto';
 import './dreamstatschat.css';
 
 const DreamStatsChart = () => {
-  
+  const [data, setData] = useState(null);
+  const [locationCharts, setLocationCharts] = useState([]);
 
+  async function fetchData() {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/dreamstat`);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const stats = data.stats;
+      const newLocationCharts = [];
+
+      // Destroy the existing chart instances
+      locationCharts.forEach((chart) => chart.destroy());
+
+      // Create a new chart for each location
+      stats.forEach((locationData) => {
+        const location = locationData.location;
+        const dreams = locationData.dreams;
+        const categories = dreams.map((dream) => dream.category);
+        const counts = dreams.map((dream) => dream.count);
+
+        const canvasId = `locationChart-${location}`;
+
+        const chart = new Chart(canvasId, {
+          type: 'pie',
+          data: {
+            labels: categories,
+            datasets: [
+              {
+                data: counts,
+                backgroundColor: [
+                  '#FF6384',
+                  '#36A2EB',
+                  '#FFCE56',
+                  '#4BC0C0',
+                  '#9966FF',
+                  '#FF9F40',
+                ],
+              },
+            ],
+          },
+        });
+
+        newLocationCharts.push(chart);
+      });
+
+      // Save the new chart instances to state
+      setLocationCharts(newLocationCharts);
+    }
+  }, [data]);
+
+  return (
+  
+ 
 };
 export default DreamStatsChart;
