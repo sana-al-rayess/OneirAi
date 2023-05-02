bc57d4a53268059b71df3235b6ad52a1bf19e5d5
Sana Rayess <rayess.sana@gmail.com>
2023-05-02 08:53:32 +0300

    frontend:created a fn to display data in bar chart

--- oneirai-frontend/src/components/BarChart/index.jsx
+++ oneirai-frontend/src/components/BarChart/index.jsx
@@ -3,7 +3,77 @@ import axios from 'axios';
 import Chart from 'chart.js/auto';
 
 const BarChart = () => {
+  const [data, setData] = useState(null);
+  const [locationCharts, setLocationCharts] = useState([]);
 
+  async function fetchData() {
+    try {
+      const response = await axios.get(`http://127.0.0.1:8000/api/dreamstat`);
+      setData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (data) {
+      const stats = data.stats;
+      const newLocationCharts = [];
+
+      // Destroy the existing chart instances
+      locationCharts.forEach((chart) => chart.destroy());
+
+      // Create a new chart for each location
+      stats.forEach((locationData) => {
+        const location = locationData.location;
+        const dreams = locationData.dreams;
+        const categories = dreams.map((dream) => dream.category);
+        const counts = dreams.map((dream) => dream.count);
+
+        const canvasId = `barChart-${location}`;
+
+        const chart = new Chart(canvasId, {
+          type: 'bar',
+          data: {
+            labels: categories,
+            datasets: [
+              {
+                label: 'Dream Counts',
+                data: counts,
+                backgroundColor: [
+                  '#FF6384',
+                  '#36A2EB',
+                  '#FFCE56',
+                  '#4BC0C0',
+                  '#9966FF',
+                  '#FF9F40',
+                ],
+              },
+            ],
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true,
+              },
+            },
+          },
+        });
+
+        newLocationCharts.push(chart);
+      });
+
+      // Save the new chart instances to state
+      setLocationCharts(newLocationCharts);
+    }
+  }, [data]);
+
+  return (
+  )
 };
 
 export default BarChart;
