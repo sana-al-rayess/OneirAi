7a8467025592157fb917d74e03b39eff408b6d52
Sana Rayess <rayess.sana@gmail.com>
2023-05-04 16:37:17 +0300

    frontend: edited subscription

--- oneirai-frontend/src/components/Subscribe/index.jsx
+++ oneirai-frontend/src/components/Subscribe/index.jsx
@@ -6,7 +6,7 @@ const Subscribe = () => {
         <div className="subscribe-container">
           <p className="subscribe-title">Get In Touch</p>
           <p className="subscribe-slogan">
-            Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero.
+          Join us to unlock the mysteries of your dreams<br /> & explore the hidden depths of your subconscious mind.
           </p>
           <form className='form-subs'>
             <input className='input-subs' type="email" id="email" placeholder="Your email" />
