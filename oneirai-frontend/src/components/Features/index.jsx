6184355f962768afd397fe25fac7494c840aa806
Sana Rayess <rayess.sana@gmail.com>
2023-05-06 16:23:02 +0300

    frontend: edited features component

--- oneirai-frontend/src/components/Features/index.jsx
+++ oneirai-frontend/src/components/Features/index.jsx
@@ -3,11 +3,13 @@ import Button from "../Button";
 import Feature1 from "../../images/Mask.png";
 import Feature2 from "../../images/transai.png";
 import Feature3 from "../../images/7248902.png"
+import Feature4 from "../../images/horo.png"
+import { Link } from "react-router-dom";
 
 import './features.css';
 
 const Features = () => {
- 
+
     return (
         <div>
             <p className="feature-title">FEATURES</p>
@@ -17,8 +19,11 @@ const Features = () => {
 
                 <div className="feature-txt-button">
                     <div><p className="feature1">Lorem Epsum Lorem Epsum <br />Lorem Epsum Lorem Epsum  </p></div>
-
-                    <div><Button>Try Now</Button></div>
+                  
+                        <Link to="/user">
+                            <div><Button>Try Now</Button></div>
+                        </Link>
+                    
                 </div>
             </div>
 
@@ -41,6 +46,16 @@ const Features = () => {
 
                     <div><Button>Try Now</Button></div>
                 </div>
+            </div>
+
+            <div className="features-container">
+
+                <div className="feature-txt-button">
+                    <div><p className="feature1">Lorem Epsum Lorem Epsum <br />Lorem Epsum Lorem Epsum  </p></div>
+
+                    <div><Button>Try Now</Button></div>
+                </div>
+                <img className="photo" src={Feature4} alt="photo" />
             </div>
         </div>
     );
