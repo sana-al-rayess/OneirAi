cf987e843cf3c0832f70355d05dfeebd0a0f3dc8
Sana Rayess <rayess.sana@gmail.com>
2023-05-06 15:51:50 +0300

    frontend: edited design in admin panel

--- oneirai-frontend/src/components/AdminSideLeft/index.jsx
+++ oneirai-frontend/src/components/AdminSideLeft/index.jsx
@@ -10,45 +10,48 @@ const AdminSideLeft = () => {
 
     return (
         <div className="admin-left">
-            <div className="admin-title"> <h1>Admin Panel</h1></div>
-            <div className="chart-row1">
-                <div className="pie-ad">
-                    <h3>Statistics</h3>
-                    <p1>View statistics in Bar Charts</p1>
-                    <Link to="/admin/barchart">
-                    <img className="chart-img-bar" src={Bar} alt="image" />
-                    </Link>
-                </div>
+            <div className='admin-panel'>
+                <div className="admin-title"> <h1>Admin Panel</h1></div>
+                <div className='chart-box'>
+                    <div className="chart-row1">
+                        <div className="pie-ad">
+                            <h3 className='yellow'>Statistics</h3>
+                            <p1>View statistics in Bar Charts</p1>
+                            <Link to="/admin/barchart">
+                                <img className="chart-img-bar" src={Bar} alt="image" />
+                            </Link>
+                        </div>
 
-                <div className="pie-ad">
-                    <h3>Statistics</h3>
-                    <p1>View statistics in Pie Charts</p1>
-                    <Link to="/admin/piechart">
-                    <img className="chart-img-pie" src={Pie} alt="image" />
-                    </Link>
-                </div>
+                        <div className="pie-ad">
+                            <h3 className='yellow'>Statistics</h3>
+                            <p1>View statistics in Pie Charts</p1>
 
+                            <Link to="/admin/piechart">
+                                <img className="chart-img-pie" src={Pie} alt="image" />
+                            </Link>
+                        </div>
+
+                    </div>
+
+                    <div className="chart-row1">
+                        <div className="pie-ad">
+                            <h3 className='yellow'>Statistics</h3>
+                            <p1>View statistics in Polar Charts</p1>
+                            <Link to="/admin/polarchart">
+                                <img className="chart-img-line" src={Polar} alt="image" />
+                            </Link>
+                        </div>
+
+                        <div className="pie-ad">
+                            <h3 className='yellow'>Statistics</h3>
+                            <p1>View statistics in Line Charts</p1>
+                            <Link to="/admin/linechart">
+                                <img className="chart-img-line" src={Line} alt="image" />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div className="chart-row1">
-                <div className="pie-ad">
-                    <h3>Statistics</h3>
-                    <p1>View statistics in Pie Charts</p1>
-                    <Link to="/admin/polarchart">
-                    <img className="chart-img-line" src={Polar} alt="image" />
-                    </Link>
-                </div>
-
-                <div className="pie-ad">
-                    <h3>Statistics</h3>
-                    <p1>View statistics in Line Charts</p1>
-                    <Link to="/admin/linechart">
-                    <img className="chart-img-line" src={Line} alt="image" />
-                    </Link>
-                </div>
-
-            </div>
-
         </div>
     );
 }
