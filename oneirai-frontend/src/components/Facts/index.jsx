521c98a7fca458ddac4f7a9e1789f714eca9644e
Sana Rayess <rayess.sana@gmail.com>
2023-05-07 23:24:57 +0300

    frontend: edited Facts

--- oneirai-frontend/src/components/Facts/index.jsx
+++ oneirai-frontend/src/components/Facts/index.jsx
@@ -7,33 +7,37 @@ const Facts = () => {
             <p className="feature-title">Did You Know?</p>
             <div class="container-cards">
                 <div class="card">
-                    <h3 class="title">Fact 1</h3>
+                    <h3 class="title-facts">Animals</h3>
                     <div class="bar">
                         <div class="emptybar"></div>
+                     <div className='fact'>Animals dream, although the exact nature of their dreams is uncertain.</div>
                         <div class="filledbar"></div>
                     </div>
 
                 </div>
                 <div class="card">
-                    <h3 class="title">Fact 2</h3>
+                    <h3 class="title-facts">Google Idea</h3>
                     <div class="bar">
                         <div class="emptybar"></div>
+                        <div className='fact'>The idea of Google came to Larry Page in one of his dreams.</div>
                         <div class="filledbar"></div>
                     </div>
 
                 </div>
                 <div class="card">
-                    <h3 class="title">Fact 3</h3>
+                    <h3 class="title-facts">Shared Dreams</h3>
                     <div class="bar">
                         <div class="emptybar"></div>
+                        <div className='fact'>There have been many instances of individuals reporting shared dreams.</div>
                         <div class="filledbar"></div>
                     </div>
                 </div>
 
                 <div class="card">
-                    <h3 class="title">Fact 4</h3>
+                    <h3 class="title-facts">Gods</h3>
                     <div class="bar">
                         <div class="emptybar"></div>
+                        <div className='fact'> Greeks and Romans believed that dreams were messages from the Gods.</div>
                         <div class="filledbar"></div>
                     </div>
 
